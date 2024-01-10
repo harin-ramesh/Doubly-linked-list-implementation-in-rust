a6125b23917be501a7df5c0e3d8000b9cb86358c
Harin Ramesh <iamharinramesh@gmail.com>
2024-01-10 19:29:25 +0530

    removed ref cycle issue

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@ use std::cell::RefCell;
 
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+type WeakLink<T> = Option<Weak<RefCell<Node<T>>>>;
 
 struct Node<T> {
   value: T,
   next: Link<T>,
-  prev: Link<T>,
+  prev: WeakLink<T>,
 }
 
 impl<T> Node<T> {
@@ -20,15 +21,15 @@ impl<T> Node<T> {
     }
 }
 
-pub struct Queue<T> {
+pub struct DoublyLinkedList<T> {
     head: Link<T>,
     tail: Link<T>,
-    size: usize
+    size: usize,
 }
 
-impl<T> Queue<T> {
+impl<T> DoublyLinkedList<T> {
     pub fn new() -> Self {
-        Self { head: None, tail: None, size:0 }
+        Self { head: None, tail: None, size:0,}
     }
 
     pub fn len(&self) -> usize {
@@ -43,7 +44,7 @@ impl<T> Queue<T> {
         let new_node = Rc::new(RefCell::new(Node::new(item)));
 
         if let Some(current_tail) = self.tail.take() {
-            new_node.borrow_mut().prev = Some(current_tail.clone());
+            new_node.borrow_mut().prev = Some(Rc::downgrade(&current_tail));
             current_tail.borrow_mut().next = Some(new_node.clone()); 
             self.tail = Some(new_node);
         } else {
@@ -58,8 +59,9 @@ impl<T> Queue<T> {
             self.size -= 1;
             match current_tail.borrow_mut().prev.take() {
                 Some(node) => {
-                    node.borrow_mut().next = None;
-                    self.tail = Some(node);
+                    let updgraded_node = node.upgrade().unwrap();
+                    updgraded_node.borrow_mut().next = None;
+                    self.tail = Some(updgraded_node);
                 }
                 None => {
                     self.head.take();
@@ -74,9 +76,10 @@ impl<T> Queue<T> {
     }
 }
 
+
 #[test]
 pub fn queue_len_test(){
-    let mut q = Queue::new();
+    let mut q = DoublyLinkedList::new();
  
     q.push(0);
     q.push(1);
@@ -89,7 +92,7 @@ pub fn queue_len_test(){
 
 #[test]
 pub fn queue_pop_test(){
-    let mut q = Queue::new();
+    let mut q = DoublyLinkedList::new();
  
     q.push(0);
     q.push(1);
